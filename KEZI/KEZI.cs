06908c8b54341070073fde5d32ce56c4653c17e9
Ole Lambaek <ole@lambaek.net>
2019-01-06 14:21:41 +0100

    Add project files.

---
+++ KEZI/KEZI.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Windows.Devices.SerialCommunication;
+using Windows.Devices.Enumeration;
+
+namespace KEZI
+{
+    public class KEZI
+    {
+        public static async void FindDevices()
+        {
+            var devices = await DeviceInformation.FindAllAsync(SerialDevice.GetDeviceSelector());
+            
+            foreach (var dev in devices)
+            {
+                System.Diagnostics.Debug.WriteLine("Id: " + dev.Id );
+                System.Diagnostics.Debug.WriteLine("Kind: " + dev.Kind );
+                System.Diagnostics.Debug.WriteLine("Name: " + dev.Name);
+                System.Diagnostics.Debug.WriteLine("");
+            }
+        }
+    }
+}
